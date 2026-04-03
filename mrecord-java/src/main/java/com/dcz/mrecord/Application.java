74e502c87ebda3e1c60ceebcdf923a43601285ee
linjuncheng <46856187@qq.com>
2026-04-03 16:58:03 +0800

    init:init java project

---
+++ mrecord-java/src/main/java/com/dcz/mrecord/Application.java
@@ -0,0 +1,17 @@
+package com.dcz.mrecord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ *
+ * @author dcz
+ * @date 2026/04/03
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
