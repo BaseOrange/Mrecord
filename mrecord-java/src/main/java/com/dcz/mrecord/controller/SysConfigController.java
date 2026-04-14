beae0f5ae1a9e7ee77680cf845a380ec20fc013f
BaseOrange <46856187@qq.com>
2026-04-14 17:50:18 +0800

    feat: Add interface to refresh configuration item cache.

--- mrecord-java/src/main/java/com/dcz/mrecord/controller/SysConfigController.java
+++ mrecord-java/src/main/java/com/dcz/mrecord/controller/SysConfigController.java
@@ -1,7 +1,11 @@
 package com.dcz.mrecord.controller;
 
+import com.dcz.mrecord.common.CheckAdmin;
+import com.dcz.mrecord.common.Result;
 import com.dcz.mrecord.service.SysConfigService;
 import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,9 +15,18 @@ import org.springframework.web.bind.annotation.RestController;
  * @author dcz
  * @since 2026/04/09
  */
+@Slf4j
 @RestController
 @RequestMapping("/config")
 public class SysConfigController {
     @Resource
     private SysConfigService sysConfigService;
+
+    @CheckAdmin
+    @PostMapping("/refreshCache")
+    public Result<String> refreshCache() {
+        log.info("刷新配置项缓存[/config/refreshCache]请求");
+        sysConfigService.refreshCache();
+        return Result.success();
+    }
 }
