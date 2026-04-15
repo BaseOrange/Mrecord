1aad78c86b7cf919ba28d3c0aa46658051fe7989
BaseOrange <46856187@qq.com>
2026-04-15 10:51:19 +0800

    init: Initialize a Vue 3 project with Vite and introduce Varlet UI dependencies.

---
+++ mrecord-vue/src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import { Button as VarButton } from '@varlet/ui'
+import '@varlet/ui/es/button/style/index'
+</script>
+
+<template>
+  <HelloWorld />
+  <var-button type="primary">Example</var-button>
+</template>
