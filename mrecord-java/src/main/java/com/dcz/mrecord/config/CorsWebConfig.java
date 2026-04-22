4fe315d3aa0048ceecb856f85a066432d982b820
BaseOrange <46856187@qq.com>
2026-04-22 15:04:40 +0800

    feat: Add cross-domain configuration support

---
+++ mrecord-java/src/main/java/com/dcz/mrecord/config/CorsWebConfig.java
@@ -0,0 +1,28 @@
+package com.dcz.mrecord.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+/**
+ * 跨域配置
+ *
+ * @author dcz
+ * @date 2026/4/22
+ */
+@Configuration
+public class CorsWebConfig {
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOriginPattern("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return new CorsWebFilter(source);
+    }
+}
