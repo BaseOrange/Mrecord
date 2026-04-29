46ca86213ec6b156f11f5770afaf425d02b251ac
BaseOrange <46856187@qq.com>
2026-04-29 16:35:47 +0800

    feat: add account book statistics related interface and data structure.

--- mrecord-vue/src/api/modules/book.ts
+++ mrecord-vue/src/api/modules/book.ts
@@ -11,6 +11,32 @@ export interface BookInfo extends BaseEntity {
     bookName?: string
 }
 
+/** 账簿统计数据 */
+export interface BookStatistics {
+    /** 账簿ID */
+    bookId?: string
+    /** 账簿名称 */
+    bookName?: string
+    /** 所属用户ID */
+    userId?: string
+    /** 统计年份 */
+    year?: number
+    /** 统计月份 */
+    month?: number
+    /** 当月总资产 */
+    totalAsset?: number
+    /** 当月总负债 */
+    totalLiability?: number
+    /** 当月净资产 */
+    netAsset?: number
+    /** 环比增长/下跌金额 */
+    monthOnMonth?: number
+    /** 同比增长/下跌金额 */
+    yearOnYear?: number
+    /** 用户本月汇总备注 */
+    note?: string
+}
+
 /** 创建/更新账簿传参 */
 export interface SaveBookParams {
     /** 账簿ID（更新时必传） */
@@ -35,6 +61,16 @@ export interface ListBooksParams extends PageParams {
     year?: string
 }
 
+/** 指定账簿详细统计响应 */
+export interface BookDetailedStatistics {
+    /** 开始年月 yyyyMM */
+    startYearMonth?: string
+    /** 结束年月 yyyyMM */
+    endYearMonth?: string
+    /** 区间财务汇总数据 */
+    recordList?: BookStatistics[]
+}
+
 // ==================== 接口方法 ====================
 
 /** 创建账簿 */
@@ -55,4 +91,14 @@ export function deleteBook(data: DeleteBookParams) {
 /** 获取账簿列表 */
 export function listBooks(data?: ListBooksParams) {
     return post<PageResult<BookInfo>>('/book/list', data)
+}
+
+/** 获取我的所有账簿统计数据 */
+export function getMyDataStatistics() {
+    return post<BookDetailedStatistics>('/book/getMyDataStatistics')
+}
+
+/** 获取指定账簿详细统计数据 */
+export function getBookDetailedStatistics(data: DeleteBookParams) {
+    return post<BookDetailedStatistics>('/book/getBookDetailedStatistics', data)
 }
